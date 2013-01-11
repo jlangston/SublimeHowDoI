06e5d5f94c267274c9b46c4b30237051d1f7f3f4
Jeff Langston <jrlangston@gmail.com>
2013-01-11 01:15:26 -0700

    Updating readme and adding multi threading and view options to plugin

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Sublime Text 2 How Do I Code Search
+# How Do I Code Search for Sublime Text 2
 
 A plugin leveraging [howdoi - a unix code search tool](https://github.com/gleitz/howdoi) terminal command to be useable from sublime.
 
@@ -25,7 +25,24 @@ At this point you have to have [howdoi](https://github.com/gleitz/howdoi) instal
       -a, --all          display the full text of the answer
       -l, --link         display only the answer link
 
+# Settings
+    Default shortcut key super + alt + h to
+
+    Settings file:
+        syntaxes: List of languages with syntax hilighting support that the plugin will try to match depending on the howdoi question
+
+        useBuffer: Show howdoi answer in quick panel or new tab
+
+
 # Disclaimer
 
-Very early proof of concept only initally tested on OSX and still in need of lots of work and polish
+This is my first go at a sublime text plugin it's only been tested on OSX and still has lots of room for improvement and polish.
+
+I'd like to get it running on windows but to this point I've had issues installing howdoi on windows. I'll look at making this plugin work under windows once I can get the command like version working in windows.
+
+# Other Notes
+
+The entire utility could be packaged up into the plugin without having to require having the command line version installed but all the python library dependencies would need to be sorted out to be included with the plugin.
+
+If you like the idea behind this plugin and would like to see improvements pull requests are welcome
 
--- SublimeHowDoI.py
+++ SublimeHowDoI.py
@@ -1,58 +1,70 @@
 # Jeff Langston
 #
-# Very early proof of concept utilizing https://github.com/gleitz/howdoi
+# Very simple proof of concept utilizing https://github.com/gleitz/howdoi to run within the editor
 
 import sublime
 import sublime_plugin
+import threading
 import commands
 # import subprocess
-# from threading import Thread
 
 #Load Settings
 settings = sublime.load_settings("SublimeHowDoI.sublime-settings")
 
 
 class HowdoiCommand(sublime_plugin.WindowCommand):
-
     def run(self):
         # Get the search term
         self.window.show_input_panel('How do I:', '',
             self.on_done, self.on_change, self.on_cancel)
 
-    # def get_anwser(self, query):
-        # p = subprocess.Popen("howdoi " + query,
-                         # stdout=subprocess.PIPE,
-                         # stderr=subprocess.STDOUT)
-        # return iter(p.stdout.readline, b'')
-
-    def handle_query(self, syntaxSettings, query):
-        instructions = commands.getstatusoutput("howdoi " + str(query))
-        # instructions = self.get_anwser(self, query)
-        syntaxes = [x.lower() for x in syntaxSettings]
-        matchedSyntax = False
-        for searchWord in query.split():
-                if searchWord.lower() in syntaxes:
-                    matchedSyntax = True
-                    searchTermSyntax = searchWord
-                    break
-
-        # Create an output buffer
-        self.output_view = self.window.new_file()
-        self.output_view.set_name(query)
-        self.output_view.set_scratch(True)
-        edit = self.output_view.begin_edit()
-        self.output_view.insert(edit, 0, instructions[1])
-        self.output_view.end_edit(edit)
-        if matchedSyntax:
-            self.output_view.set_syntax_file("Packages/" + searchTermSyntax + "/" + searchTermSyntax + ".tmLanguage")
-
     def on_done(self, input):
-        # t = Thread(target=lambda: sublime.set_timeout(self.handle_query(settings.get("syntaxes"), input), 100), args=())
-        # t.start()
-        self.handle_query(settings.get("syntaxes"), input)
+        self.cmdThread(settings.get("syntaxes"), input, settings.get("useBuffer"), self.window).start()
 
     def on_change(self, input):
         pass
 
     def on_cancel(self):
         pass
+
+    # Seperate thread to run howdoi query in background
+    class cmdThread(threading.Thread):
+        def __init__(self, syntaxes, query, useBuffer, window):
+            self._syntaxes = syntaxes
+            self._query = query
+            self._useBuffer = useBuffer
+            self._window = window
+            threading.Thread.__init__(self)
+
+        def run(self):
+            query = self._query
+            # p = subprocess.Popen("howdoi " + query, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
+            # stdoutdata = p.communicate().get(0)
+            query_response = commands.getstatusoutput("howdoi " + query)
+            syntaxes = [x.lower() for x in self._syntaxes]
+            matchedSyntax = False
+            for searchWord in query.split():
+                    if searchWord.lower() in syntaxes:
+                        matchedSyntax = True
+                        searchTermSyntax = searchWord
+                        break
+            sublime.set_timeout(lambda: self.show_answer(query, matchedSyntax, query_response, searchTermSyntax, self._useBuffer, self._window), 0)
+
+        # Create an output buffer
+        def show_answer(self, query, matchedSyntax, query_response, searchTermSyntax, useBuffer, window):
+            if useBuffer:
+                output_view = window.new_file()
+                output_view.set_name(query)
+                output_view.set_scratch(True)
+                edit = output_view.begin_edit()
+                output_view.insert(edit, 0, query_response[1])
+                output_view.end_edit(edit)
+            else:
+                output_view = window.get_output_panel('howdoi_answer')
+                edit = output_view.begin_edit()
+                output_view.erase(edit, sublime.Region(0, output_view.size()))
+                output_view.insert(edit, 0, query_response[1])
+                output_view.end_edit(edit)
+                window.run_command('show_panel', {"panel": 'output.howdoi_answer'})
+            if matchedSyntax:
+                output_view.set_syntax_file("Packages/" + searchTermSyntax + "/" + searchTermSyntax + ".tmLanguage")
